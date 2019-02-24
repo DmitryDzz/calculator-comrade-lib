900fd566ae35e1b238dc2d2a8bcadc5a62b28674
DmitryDzz <DzakhovDS@prosense.tv>
2019-02-24 17:48:46 +0300

    Resolved #26 Android x86 architecture

--- build.cmd
+++ build.cmd
@@ -1,19 +1,29 @@
 @echo off
 
-set PLATFORM=arm64-v8a
-echo[ & echo Platform %PLATFORM% & echo ------------------ & echo[
+rem Android
+rem -------
+
+set PLATFORM=android-arm64-v8a
+echo[ & echo Platform %PLATFORM% & echo -------------------------- & echo[
 call build-platform.cmd %PLATFORM%
 
-set PLATFORM=armeabi-v7a
+set PLATFORM=android-armeabi-v7a
+echo[ & echo Platform %PLATFORM% & echo ---------------------------- & echo[
+call build-platform.cmd %PLATFORM%
+
+set PLATFORM=android-x86
 echo[ & echo Platform %PLATFORM% & echo -------------------- & echo[
 call build-platform.cmd %PLATFORM%
 
-set PLATFORM=win64
-echo[ & echo Platform %PLATFORM% & echo -------------- & echo[
+rem Windows
+rem -------
+
+set PLATFORM=windows-x86_64
+echo[ & echo Platform %PLATFORM% & echo ----------------------- & echo[
 call build-platform.cmd %PLATFORM%
 
-set PLATFORM=win32
-echo[ & echo Platform %PLATFORM% & echo -------------- & echo[
+set PLATFORM=windows-x86
+echo[ & echo Platform %PLATFORM% & echo -------------------- & echo[
 call build-platform.cmd %PLATFORM%
 
 echo[ & echo Hope it works. Press something... & pause > nul
